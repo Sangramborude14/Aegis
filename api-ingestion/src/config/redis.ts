cccaa214e1a94d7f362ee73c54053ffb1851389d
Sangram <sangram2mail@gmail.com>
2026-06-01 10:17:07 +0530

    redis configration setup

---
+++ api-ingestion/src/config/redis.ts
@@ -0,0 +1,16 @@
+import Redis from 'ioredis';
+
+export const redis = new Redis({
+    host: process.env.REDIS_HOST || 'localhost',
+    port: parseInt(process.env.REDIS_PORT || '6379', 10),
+    maxRetriesPerRequest: null,
+    
+})
+
+redis.on('connect',() => {
+    console.log(`Redis connected Successfully`);
+});
+
+redis.on('error',(err) => {
+    console.error('Redis Connection Error',err);
+});
